a1c350af6259d2c137b869f43005223219a444e9
Tsuplige <thomassupligeon@gmail.com>
2024-06-03 01:46:09 +0200

    add on click effect

--- script/main.js
+++ script/main.js
@@ -1,1 +1,15 @@
-const modal = getElementById("modal")
+
+
+function ModalAppear(){
+    let modal = document.getElementById("modal").style.display = "flex"
+}
+
+function ModalDisAppear(){
+    let modal = document.getElementById("modal").style.display = "none"
+}
+
+function MakeChoice(choice){
+    
+}
+
+onclick
